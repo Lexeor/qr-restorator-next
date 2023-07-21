b76bcae36f0b708dce68aa192ffba1673fe26363
Lexeor <lexeor.ross@gmail.com>
2023-07-21 18:17:59 +0400

    fix sidebar links visuals

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,13 +1,37 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import menuItems from "./items";
+
+// import DashboardIcon from "@mui/icons-material/Dashboard";
+// import TableRestaurantIcon from "@mui/icons-material/TableRestaurant";
+// import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+// import AssessmentIcon from "@mui/icons-material/Assessment";
+// import StorefrontIcon from "@mui/icons-material/Storefront";
+
+// const icons = [
+//   DashboardIcon,
+//   TableRestaurantIcon,
+//   RestaurantMenuIcon,
+//   AssessmentIcon,
+//   StorefrontIcon,
+// ];
 
 type Props = {};
 
 function Sidebar({}: Props) {
+  const currentRoute = usePathname();
+
   const renderItems = menuItems.map((item) => {
     return (
-      <Link key={item.id} href={item.path}>
+      <Link
+        key={item.id}
+        href={item.path}
+        className={`menu-item ${currentRoute === item.path ? "active" : ""}`}
+      >
+        <i className={item.icon}></i>
         {item.title}
       </Link>
     );
