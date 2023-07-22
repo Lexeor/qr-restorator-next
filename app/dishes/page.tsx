69688b7a495abbb12ad3e17c6effbe8d2cf70901
Lexeor <lexeor.ross@gmail.com>
2023-07-22 17:32:22 +0400

    add Dishes tab

---
+++ app/dishes/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Dishes({}: Props) {
+  return <div>Dishes</div>;
+}
+
+export default Dishes;
--- components/Sidebar/items.ts
+++ components/Sidebar/items.ts
@@ -9,8 +9,9 @@ const items: MenuItem[] = [
   { id: 0, path: "/dashboard", title: "Dashboard", icon: "Dashboard" },
   { id: 1, path: "/tables", title: "Tables", icon: "TableRestaurant" },
   { id: 2, path: "/menu", title: "Menu", icon: "RestaurantMenu" },
-  { id: 3, path: "/reports", title: "Reports", icon: "Assessment" },
-  { id: 4, path: "/restaurant", title: "Restaurant", icon: "Storefront" },
+  { id: 4, path: "/dishes", title: "Dishes", icon: "BrunchDining" },
+  { id: 5, path: "/reports", title: "Reports", icon: "Assessment" },
+  { id: 6, path: "/restaurant", title: "Restaurant", icon: "Storefront" },
 ];
 
 export default items;
